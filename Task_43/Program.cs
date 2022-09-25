cb845c0563c7a8419328ecafbee32d007dfb49c0
Olesya <lesusha1@yandex.ru>
2022-09-26 02:36:43 +0300

    add Task_43

---
+++ Task_43/Program.cs
@@ -0,0 +1,39 @@
+﻿// 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, 
+// b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите значение b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+GetCrossPoint(b1, k1, b2, k2);
+
+
+void GetCrossPoint(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2) Console.WriteLine("Две прямые параллельны");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"({x}; {y})");
+    }
+}
+
+
+// Console.Write("Введите значения b1, k1, b2, k2 через пробел: ");
+// string input = Console.ReadLine()!;
+// string[] numbers = input.Split(' ');
+// double b1 = double.Parse(numbers[0]);
+// double k1 = double.Parse(numbers[1]);
+// double b2 = double.Parse(numbers[2]);
+// double k2 = double.Parse(numbers[3]);
+
+// double x = (b2 - b1) / (k1 - k2);
+// double y = k1 * x + b1;
+// Console.WriteLine($"({x}; {y})");
